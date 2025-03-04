5ae6afb83835a8407884e3da43385f1bcf88462f
trueHyper <volkoofsenya@gmail.com>
2025-03-04 16:44:09 +0300

    grdp-scan.go

--- grdp-scan.go
+++ grdp-scan.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net"
@@ -9,7 +10,7 @@ import (
 	"strings"
 	"encoding/binary"
 	"time"
-
+	"os"
 )
 
 type NTLMChallenge struct {
@@ -28,40 +29,60 @@ const (
 	ntEpochOffset = 11644473600
 )
 
-// wireshark
+// **** Wireshark Packet ****
 //char peer0_0[] = 
 //{ /* Packet 4 */
-//	0x03, 0x00, 0x00, 0x13,  TPKT Header (версия 3, длина 19 байт)
-//	0x0e, 					 
-//	0xe0, 
-//	0x00, 0x00, 
-//	0x00, 0x00, 
-//	0x00, 
-//	0x01, 0x00, 0x08,  		 RDP Negotiation Request (Type: 1, Flags: 0, len: 8)
-//	0x00, 0x0b, 0x00,  		 Requested Protocols (PROTOCOL_RDP | PROTOCOL_SSL | PROTOCOL_HYBRID) а где четвертый?
-//	0x00, 0x00 		         Padding ??
+//  0x03, 0x00, 0x00, 0x13, 
+//  0x0e, 0xe0, 0x00, 0x00, 
+//  0x00, 0x00, 0x00, 0x01, 
+//  0x00, 0x08, 0x00, 0x0b, 
+//  0x00, 0x00, 0x00 				 
 //};
 
 func main() {
-	server := "157.90.41.185:3389" 
-	conn, err := net.Dial("tcp", server)
-	if err != nil {
-		log.Fatalf("Ошибка подключения: %v", err)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Using: go run main.go <addr:port>")
+		return
 	}
+
+	server := os.Args[1]
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second) // 2sec timeout
+	defer cancel()
+
+	var conn net.Conn
+	var err error
+	done := make(chan bool)
+
+	go func() {
+		conn, err = net.Dial("tcp", server)
+		done <- true
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("Connection error: waiting time exceeded (2 секунды)")
+		return
+	case <-done:
+		if err != nil {
+			log.Fatalf("Connection error: %v", err)
+		}
+	}
+
 	defer conn.Close()
 
 	// X.224 Connection Request PDU (TPKT + X.224)
-	syncPacket := []byte {
-	
-		0x03, 0x00, 0x00, 0x13,  // TPKT Header (версия 3, длина 19 байт) //19 c
-		0x0e, 			 // X.224 Length Indicator //14 без
-		0xe0,                    // X.224 CR(0xe) CDT(0x0) -> 0xe
-		0x00, 0x00,              // X.224 DST-REF
-		0x00, 0x00,              // X.224 SRC-REF 
-		0x00,                    // X.224 CLASS OPTION
-		0x01, 0x00, 0x0008,  	 // X.224 RDP Negotiation Request (Type: 0x01, Flags: 0x00, Length: 0x08)
-		0x00, 0x0b, 0x00, 0x00,  // Requested Protocols (PROTOCOL_RDP | PROTOCOL_SSL | PROTOCOL_HYBRID) 
-		0x00,			 // Padding
+	syncPacket := []byte{
+		0x03, 0x00, 0x00, 0x13, // TPKT Header (версия 3, длина 19 байт)
+		0x0e,                   // X.224 Length Indicator
+		0xe0,                   // X.224 CR(0xe) CDT(0x0) -> 0xe
+		0x00, 0x00,             // X.224 DST-REF
+		0x00, 0x00,             // X.224 SRC-REF
+		0x00,                   // X.224 CLASS OPTION
+		0x01, 0x00, 0x08,       // X.224 RDP Negotiation Request (Type: 0x01, Flags: 0x00, Length: 0x08)
+		0x00, 0x0b, 0x00, 0x00, // Requested Protocols (PROTOCOL_RDP | PROTOCOL_SSL | PROTOCOL_HYBRID)
+		0x00,                   // Padding
 	}
 
 	_, err = conn.Write(syncPacket)
@@ -77,13 +98,14 @@ func main() {
 	}
 
 	fmt.Printf("\nServer response\n%s", hex.Dump(buffer[:n]))
-	
-	// switch to tls cipher
+
+	// switch to TLS
 	tlsConn := tls.Client(conn, &tls.Config{InsecureSkipVerify: true})
 	defer tlsConn.Close()
-	
-	ntlmNegotiate := []byte {
-		0x30, 0x37, 0xA0, 0x03, 0x02, 0x01, 0x60, 0xA1, 0x30, 0x30, 0x2E, 0x30, 0x2C, 0xA0, 0x2A, 0x04, 0x28, // ASN header
+
+	// NTLM Negotiate
+	ntlmNegotiate := []byte{
+		0x30, 0x37, 0xA0, 0x03, 0x02, 0x01, 0x60, 0xA1, 0x30, 0x30, 0x2E, 0x30, 0x2C, 0xA0, 0x2A, 0x04, 0x28, // ASN
 		0x4E, 0x54, 0x4C, 0x4D, 0x53, 0x53, 0x50, 0x00, // "NTLMSSP" Signature
 		0x01, 0x00, 0x00, 0x00, // NTLM Message Type 1
 		0xB7, 0x82, 0x08, 0xE2, // Flags
@@ -94,51 +116,43 @@ func main() {
 		0x00, 0x00, 0x00, // Reserved
 		0x0F, // NTLM Revision
 	}
-	
+
 	_, err = tlsConn.Write(ntlmNegotiate)
 	if err != nil {
 		fmt.Println("Error sending NTLM Negotiate:", err)
 		return
-	}	
-	
+	}
+
 	n, err = tlsConn.Read(buffer)
 	if err != nil {
 		fmt.Println("NTLM Challenge Reading Error:", err)
 		return
 	}
-	
-	//fmt.Println("NTLM Challenge:", buffer[:n])
-	
-	parseNTLMChallenge(buffer[:n])
+
+	printNTLMTargetInfo(buffer[:n])
 }
 
-
-func parseNTLMChallenge(challenge []byte) {
-	if len(challenge) < 48 {
-		fmt.Println("Error: NTLM Challenge is too short")
-		return
-	}
-
-	fmt.Printf("\nTarget Info\n%s", hex.Dump(challenge))
-	
-	printNTLMTargetInfo(challenge)
-}
-
-func printNTLMTargetInfo(data []byte) {
+func printNTLMTargetInfo(data []byte) error {
 	
 	var challenge NTLMChallenge
 	var targetLen, targetOffset int
 	
+	//fmt.Printf("\nTarget Info\n%s", hex.Dump(challenge))
+	
 	pos := strings.Index(string(data), signature)
 	if pos == -1 {
-		//return nil, fmt.Errorf("NTLM signature not found")
+		return fmt.Errorf("NTLM signature not found")
 	}
 	
 	buffer := data[pos:] // discard ASN header
 	
+	if len(buffer) < 48 {
+		return fmt.Errorf("Error: NTLM Challenge is too short")
+	}
+	
 	msgType := int(binary.LittleEndian.Uint16(buffer[8:12]))  // 4byte LE
 	if msgType != 0x2 { 
-		//return nil, fmt.Errorf("NTLM Challenge (Type 2) was expected, type received %d", messageType)
+		return fmt.Errorf("NTLM Challenge (Type 2) was expected, type received %d", msgType)
 	}
 	
 	targetLen =  int(binary.LittleEndian.Uint16(buffer[12:14]))  // 2byte LE
@@ -147,7 +161,6 @@ func printNTLMTargetInfo(data []byte) {
 	if targetLen > 0 {
 	// get TargetName
 		challenge.TargetName = string(removeNullBytes(buffer[targetOffset:targetOffset + targetLen]))
-		//fmt.Println(challenge.TargetName)
 	}
 	
 	var domainLen, domainOffset int
@@ -155,18 +168,14 @@ func printNTLMTargetInfo(data []byte) {
 	domainLen = int(binary.LittleEndian.Uint16(buffer[40:42])) 
 	domainOffset = int(binary.LittleEndian.Uint16(buffer[44:48])) 
 	
-	// win95 check...?
+	// win9x check...?
 	
 	// get version major/minor/build
 	challenge.ProductVersion = fmt.Sprintf("%d.%d.%d", buffer[48], buffer[49], 
 					int(binary.LittleEndian.Uint16(buffer[50:52])))
 	
-	//fmt.Println("ver:",challenge.ProductVersion)
-	
 	if domainLen == 0 {
-	// no TargetInfoFields
-		//return
-		fmt.Printf("domainLen = 0")
+		return fmt.Errorf("Domain Length 0, target TargetInfo empty")
 	}
 	
 	var dataLen, offset, fieldType, fieldLen int
@@ -221,6 +230,8 @@ func printNTLMTargetInfo(data []byte) {
 	fmt.Println("|DNS_Tree_Name:", challenge.DNSTreeName)
 	fmt.Println("|Product_Version:", challenge.ProductVersion)
 	fmt.Println("|System_Time:", challenge.SystemTime)
+	
+	return nil
 }
 
 func removeNullBytes(data []byte) []byte {
@@ -246,4 +257,3 @@ func ConvertFILETIME(filetime []byte) string {
 	
 	return t.Format(time.RFC3339)
 }
-
